0949a17375e4f8b140c2d32a5de1b3f7de69c4cc
schauh08 <schauh08@nyit.edu>
2025-06-03 14:15:35 -0400

    Updated ingestion script.

--- src/etl/ingest_raw.py
+++ src/etl/ingest_raw.py
@@ -1,9 +1,15 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import os
 
-engine = create_engine('sqlite:///churn.db')
+script_dir = os.path.dirname(__file__)
+project_root = os.path.abspath(os.path.join(script_dir, os.pardir, os.pardir))
+csv_path = os.path.join(project_root, 'data', 'raw', 'WA_Fn-UseC_-Telco-Customer-Churn.csv')
 
-churn_df = pd.read_csv('WA_Fn-UseC_-Telco-Customer-Churn.csv')
+db_path = os.path.join(project_root, 'churn.db')
+engine = create_engine('sqlite:///churn.db', echo=false)
+
+churn_df = pd.read_csv(csv_path)
 
 churn_df.to_sql('raw_churn', engine, if_exists='replace', index=false)
 
