3c00dd46e388b417985b7acdead547f5515c0947
schauh08 <schauh08@nyit.edu>
2025-06-07 19:16:29 -0400

    Fixed typo.

--- src/features/pipeline.py
+++ src/features/pipeline.py
@@ -21,7 +21,7 @@ def build_feature_pipeline():
     categorical_features = ['InternetService','Contract', 'PaymentMethod', 'tenure_bucket', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV',                   'StreamingMovies']
 
     numeric_transformer = StandardScaler()
-    categorical_transformer = OneHotEncoder(drop='first',spare=False)
+    categorical_transformer = OneHotEncoder(drop='first',sparse=False)
 
     preprocessor = ColumnTransformer(transformer=[
         ('num', numeric_transformer, numeric_features),
