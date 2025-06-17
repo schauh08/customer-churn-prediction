e0bbf5dabda95c5c0057f28407f157e3c2f5a3b5
schauh08 <schauh08@nyit.edu>
2025-06-17 13:12:11 -0400

    Directed app.py to point to src

--- src/api/app.py
+++ src/api/app.py
@@ -1,3 +1,13 @@
+import os
+import sys
+
+project_root = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+)
+
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
 import joblib
 import pandas as pd
 from flask import Flask, request, jsonify
